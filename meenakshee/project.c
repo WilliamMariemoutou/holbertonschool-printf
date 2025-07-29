bf7493c0aa87698ba9dfe5d72c249d70d6307849
meenakshee23 <meenaksheegopaul@gmail.com>
2025-07-29 14:28:30 +0400

    meenakshee dir

--- meenakshee/project.c
+++ meenakshee/project.c
@@ -1,8 +1,29 @@
 #include "main.h"
-
+#include <stdargs.h>
+#include <unistd.h>
 /**
- * c: prints th character
+ * _printf - produces the output according to a format
+ * @format: character string
  *
+ * Return: number of character printed
  */
-int _printf(const char *format, ...)
 
+int _printf(const char *format, ...)
+{
+	va_list(args);
+	va_start(args, format);
+	int i = 0;
+	int count = 0;
+
+	while (format[i] != '\0')
+	{
+
+		write(1, &format[i], 1);
+		count++;
+		i++;
+	}
+
+
+	va_end(args);
+	return (count);
+}
